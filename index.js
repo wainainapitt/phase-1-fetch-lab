3464f491ef02d211c82ca4b5e1970de381fe6b62
peter <pngugi.wainaina@gmail.com>
2023-08-06 23:01:15 +0300

    done for this one

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(renderBookData =>renderBooks(renderBookData))
 }
 
+fetchBooks()
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+  books.forEach(book => 
+function renderBooks(books) {
     main.appendChild(h2);
   });
 }
 
+renderBooks()
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
